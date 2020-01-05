c3d385044d0475cfc18fe9f19e81e212ba766999
lynnkoo <lynnkoo.1120@gmail.com>
2020-01-05 14:19:58 +0800

    add common slice

--- src/logics/index.ts
+++ src/logics/index.ts
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux'
-import { accountReducer } from './account/reducers'
+import { createSlice } from 'redux-async-kit'
+import { accountReducer } from './account'
 
 export const commonReducer = {
   commonAccount: accountReducer,
 }
+
+export const commonSlice = createSlice('common', {
+  account: accountReducer,
+})
 export { accountReducer }
